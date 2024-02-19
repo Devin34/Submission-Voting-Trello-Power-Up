62ccbe748a63fb339053537721f1a96266a8fde6
Devin34 <halld35n@gmail.com>
2024-02-19 18:52:42 -0500

    idk

--- score.js
+++ score.js
@@ -17,7 +17,7 @@ t.render(function () {
   return t
     .card("id")
     .then(function(cardidvalue) {
-      console.log(cardidvalue.id);
+      console.log(cardidvalue);
     })
     .get("member", "shared", "score")
     .then(function (score) {
