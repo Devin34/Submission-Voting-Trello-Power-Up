bc4fdd0290b2e96a5c6a27ca6b47e664166236b3
Devin34 <halld35n@gmail.com>
2024-02-19 16:23:53 -0500

    fix: forgot to add id some where

--- score.js
+++ score.js
@@ -1,12 +1,12 @@
 /* global TrelloPowerUp */
 
 var t = TrelloPowerUp.iframe();
-var cardname = t.card("id");
+var cardid = t.card("id");
 
 window.scoreform.addEventListener("submit", function (event) {
   event.preventDefault();
   return t
-    .set("member", "shared", "score", window.scoreselect.value)
+    .set("member", "shared", cardid, window.scoreselect.value)
     .then(function () {
       t.closePopup();
     });
@@ -14,7 +14,7 @@ window.scoreform.addEventListener("submit", function (event) {
 
 t.render(function () {
   return t
-    .get("member", "shared", cardname)
+    .get("member", "shared", cardid)
     .then(function (score) {
       window.scoreselect.value = score;
     })
