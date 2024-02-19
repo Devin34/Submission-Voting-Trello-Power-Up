baaed4c8acdbb35073a393ba78b2d4726f8c7539
Devin34 <halld35n@gmail.com>
2024-02-19 12:37:45 -0500

    fix: Removed Select Element

--- score.js
+++ score.js
@@ -5,22 +5,12 @@ var t = TrelloPowerUp.iframe();
 window.scoreForm.addEventListener("submit", function (event) {
   event.preventDefault();
   return t
-
-
-
-    .set("card", "shared", "scoreForm", window.scoreSelect.value)
+    .set("card", "shared", "scoreForm", window.scoreForm.value)
     .then(function () {
       t.closePopup();
     });
 });
 
 t.render(function () {
-  return t
-    .get("card", "shared", "scoreForm")
-    .then(function (scoreForm) {
-      window.scoreSelect.value = scoreForm;
-    })
-    .then(function () {
-      t.sizeTo("#scorForm").done();
-    });
-})
+  t.sizeTo("#scoreForm").done();
+});
