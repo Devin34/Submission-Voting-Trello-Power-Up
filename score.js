7d482630c2421273c3997e43ed3b98bb124de9c2
Devin34 <halld35n@gmail.com>
2024-02-19 13:40:13 -0500

    fix: another one bites the dust

--- score.js
+++ score.js
@@ -2,10 +2,10 @@
 
 var t = TrelloPowerUp.iframe();
 
-window.scoreform.addEventListener("submit", function (event) {
+document.getElementById("scoreform").addEventListener("submit", function (event) {
   event.preventDefault();
   return t
-    .set("card", "shared", "score", window.scoreselect.value)
+    .set("card", "shared", "score", document.getElementById("scoreselect").value)
     .then(function () {
       t.closePopup();
     });
@@ -16,7 +16,7 @@ t.render(function () {
   return t
     .get("card", "shared", "score")
     .then(function (data) {
-      window.scoreselect.value = data;
+      document.getElementById("scoreselect").value = data;
     })
     .then(function () {
       t.sizeTo("#scoreform").done();
