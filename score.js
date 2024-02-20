7cc6c4848973320e2265f189c7740747051d0846
Devin34 <halld35n@gmail.com>
2024-02-19 23:42:32 -0500

    madesto thats where my dog comes from

--- score.js
+++ score.js
@@ -7,12 +7,14 @@ var t = TrelloPowerUp.iframe();
 window.scoreform.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  t.card("id").then(function (cardID) { t.set("member", "shared", cardID.id, window.scoreselect.value);});
+  return t.card("id").then(function (cardID) { t.set("member", "shared", cardID.id, window.scoreselect.value).then(function () {console.log("I RUN");})});
 
+  /*
   return t
   .card("id")
   .then(function (cardID) { t.get("member", "shared", cardID.id).then(function (score) {console.log(score);})})
   .then(function () { t.closePopup();});
+  */
 
 });
 
