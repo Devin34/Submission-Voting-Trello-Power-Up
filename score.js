4c4d681a2c76851c0b9c378cf45e1cf41afd51d5
Devin34 <halld35n@gmail.com>
2024-02-20 00:11:39 -0500

    It works

--- score.js
+++ score.js
@@ -10,18 +10,16 @@ window.scoreform.addEventListener("submit", function (event) {
 
   t.card("id").then(function (cardID) {
 
-  
     t.set("member", "shared", cardID.id, window.scoreselect.value)
-    .then(function () { console.log("I RUN"); })
-    .then(function () {
+      .then(function () { console.log("I RUN"); })
+      .then(function () {
 
+        state = true;
 
-      state = true;
-      //t.get("member", "shared", cardID.id).then(function (score) { console.log(score); });
+        t.closePopup();
 
-    });
- 
-  
+      });
+
   });
 
   return t;
@@ -35,16 +33,16 @@ t.render(function () {
   if (state = true) {
     t.card("id").then(function (cardID) {
 
-  
+
       t.get("member", "shared", cardID.id)
-      .then(function (score) { window.scoreselect.value = score; console.log(score); })
-      .then(function () {
-  
-        //t.get("member", "shared", cardID.id).then(function (score) { console.log(score); });
-  
-      });
-   
-    
+        .then(function (score) { window.scoreselect.value = score; console.log(score); })
+        .then(function () {
+
+          //t.get("member", "shared", cardID.id).then(function (score) { console.log(score); });
+
+        });
+
+
     });
 
     return t.sizeTo("#scoreform").done();
