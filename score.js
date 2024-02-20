9cf9cbc6148a4fac7058061e42d13d83e057f16e
Devin34 <halld35n@gmail.com>
2024-02-19 22:57:22 -0500

    hope and pray

--- score.js
+++ score.js
@@ -7,26 +7,19 @@ var t = TrelloPowerUp.iframe();
 window.scoreform.addEventListener("submit", function (event) {
   event.preventDefault();
   return t
-    .set("member", "shared", "score", window.scoreselect.value)
-    .then(function () {
-      t.closePopup();
-    });
+
+    .card("id")
+    .then(function (cardID) { t.set("member", "shared", cardID.id, window.scoreselect.value)})
+    .then(function () { t.closePopup();});
+
 });
 
 t.render(function () {
   return t
-    .get("member", "shared", "score")
-    .then(function (score) {
 
-      var key;
-      t.card("id").then(function (cardID) { key = cardID.id}).done();
-
-      console.log(typeof key);
-
-      window.scoreselect.value = score;
-
-    })
-    .then(function () {
-      t.sizeTo("#scoreform").done();
-    });
+  .card("id")
+  .then(function (cardID) {t.get("member", "shared", cardID.id)})
+  .then(function (score) {window.scoreselect.value = score;})
+  .then(function () {t.sizeTo("#scoreform").done();});
+  
 });
