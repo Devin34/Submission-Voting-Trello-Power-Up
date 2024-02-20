54b1a114552d6e0a6450b4abfdd7fffb760a50d1
Devin34 <halld35n@gmail.com>
2024-02-19 23:32:44 -0500

    weeeeeeeeeeeeeeeeeeeee

--- score.js
+++ score.js
@@ -6,13 +6,13 @@ var t = TrelloPowerUp.iframe();
 
 window.scoreform.addEventListener("submit", function (event) {
   event.preventDefault();
-  return t
 
-    .card("id")
-    .then(function (cardID) { t.set("member", "shared", cardID.id, window.scoreselect.value)})
-    .get("member", "shared", cardID.id)
-    .then(function (score) {console.log(score);})
-    .then(function () { t.closePopup();});
+  t.card("id").then(function (cardID) { t.set("member", "shared", cardID.id, window.scoreselect.value);})
+
+  return t
+  .card("id")
+  .then(function (cardID) { t.get("member", "shared", cardID.id).then(function (score) {console.log(score);})})
+  .then(function () { t.closePopup();});
 
 });
 
