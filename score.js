a6eaa9bf0b0dc990a1027c0b66e04f2c60fe7118
Devin34 <halld35n@gmail.com>
2024-02-19 23:02:39 -0500

    tears like rivers flow

--- score.js
+++ score.js
@@ -15,11 +15,14 @@ window.scoreform.addEventListener("submit", function (event) {
 });
 
 t.render(function () {
-  return t
 
+  return t.sizeTo("#scoreform").done();
+
+  /*
   .card("id")
   .then(function (cardID) {t.get("member", "shared", cardID.id)})
   .then(function (score) {window.scoreselect.value = score;})
   .then(function () {t.sizeTo("#scoreform").done();});
-  
+  */
+
 });
